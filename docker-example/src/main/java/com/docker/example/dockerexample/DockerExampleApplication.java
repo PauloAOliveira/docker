8218824409efc67731b827fd61471ecc22d46a74
Paulo Oliveira <pauloadoliveira@gmail.com>
2017-12-19 23:03:38 -0200

    Changed to entrypoint, added log to work with volumes later

--- docker-example/src/main/java/com/docker/example/dockerexample/DockerExampleApplication.java
+++ docker-example/src/main/java/com/docker/example/dockerexample/DockerExampleApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class DockerExampleApplication {
 
@@ -16,8 +18,11 @@ public class DockerExampleApplication {
 @RestController
 class Controller {
 
+	private final Logger LOGGER = Logger.getLogger("Controller");
+
 	@GetMapping
 	public String hello() {
+		LOGGER.info("Hello from log");
 		return "Hello World";
 	}
 }
